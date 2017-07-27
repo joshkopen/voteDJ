75233d7f37a892e91f8da0c569bcb684d3f5d502
Joshua Kopen <jmk86@duke.edu>
2017-07-27 17:11:11 -0400

    Headers

---
+++ voteDJ/BridgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BridgeViewController.h
+//  CrowdDJ
+//
+//  Created by Joshua Kopen on 7/12/17.
+//  Copyright © 2017 Joshua Kopen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BridgeViewController : UIStackView
+
+@end
---
+++ voteDJ/BridgeViewController.m
@@ -0,0 +1,21 @@
+//
+//  BridgeViewController.m
+//  CrowdDJ
+//
+//  Created by Joshua Kopen on 7/12/17.
+//  Copyright © 2017 Joshua Kopen. All rights reserved.
+//
+
+#import "BridgeViewController.h"
+
+@implementation BridgeViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
